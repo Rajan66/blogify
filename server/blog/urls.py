acd581a98117a7d9e0f94317160595e2c54c0691
Rajan66 <rajanmaharjan042@gmail.com>
2025-02-18 14:09:55 +0545

    feat: add urls and views for category, post

--- server/blog/urls.py
+++ server/blog/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("category/", views.category_list, name="category_list"),
+    path("category/<int:pk>/", views.category_detail, name="category_detail"),
+    path("post/", views.post_list, name="post_list"),
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
+]
